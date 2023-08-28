d3c3d4971cda6ab9bfcecdb5da79636db4d357f9
Kix300 <54603999+kix300@users.noreply.github.com>
2023-08-28 12:28:50 +0200

    all comment good

--- Shaker_mainapp.py
+++ Shaker_mainapp.py
@@ -10,12 +10,14 @@ import datetime
 import serial.tools.list_ports
 
 
-# Creation de l'app
+# Creation de l'application et de la fenetre
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("Projet")
 window.showMaximized() 
 window.setStyleSheet("background-color: ChartThemeDark; color: white;")
+
+
 
 # Creation des couleurs 
 palette = QPalette() 
@@ -29,13 +31,17 @@ palette.setColor(QPalette.ColorRole.Text, QColor(250, 250, 250))
 palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53)) 
 palette.setColor(QPalette.ColorRole.ButtonText, QColor(250, 250, 250)) 
 palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 110, 177)) 
-palette.setColor(QPalette.ColorRole.HighlightedText, QColor(250, 250, 250)) 
+palette.setColor(QPalette.ColorRole.HighlightedText, QColor(250, 250, 250))
+
+
 
 # Creation du graphique
 chart = QChart()
 chart.setTheme(QChart.ChartTheme.ChartThemeDark)
 
-# Creation de 3 series pour le graph
+
+
+# Creation de 3 series pour le graphique
 series1 = QLineSeries()
 series1.setName("Axe X")
 series2 = QLineSeries()
@@ -43,10 +49,14 @@ series2.setName("Axe Y")
 series3 = QLineSeries()
 series3.setName("Axe Z")
 
+
+# Affichage du graphique
 chartview = QChartView(chart)
 chartview.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-# Creation des infos autour du grphique
+
+
+# Creation des infos autour du graphique
 axis_x_box = QTextEdit()
 axis_x_box.setReadOnly(True)
 axis_x_box.setStyleSheet("background-color: ChartThemeDark; color: white; font-size: 14px;")
@@ -64,26 +74,21 @@ axis_z_box.setFixedWidth(150)
 
 
 
-
-ports = serial.tools.list_ports.comports()
-OM1_list = []
-for p, d, h in sorted(ports):
-    OM1_list.append(p)
-
-
+# Creation du layout vertical et horizontal 
 layout = QVBoxLayout()
 h_layout = QHBoxLayout()
 h_layout.addWidget(chartview, 4)
 spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 h_layout.addItem(spacer)
+
+
+
+# Creation de 3 colonnes x, y et z 
 axis_x = QVBoxLayout()
 axis_x_title = QLabel("Axe X")
 axis_x_title.setStyleSheet("background-color: ChartThemeDark; color: white font-size: 18px;")
 axis_x.addWidget(axis_x_title)
 axis_x.addWidget(axis_x_box)
-
-
-
 
 axis_y = QVBoxLayout()
 axis_y_title = QLabel("Axe Y")
@@ -94,24 +99,32 @@ axis_y.addWidget(axis_y_box)
 axis_z = QVBoxLayout()
 axis_z_title = QLabel("Axe Z")
 axis_z_title.setStyleSheet("background-color: ChartThemeDark; color: white font-size: 18px;")
-
 axis_z.addWidget(axis_z_title)
 axis_z.addWidget(axis_z_box)
 
+
+
+# Ajout des 3 colonnes dans le layout horizontal
 h_layout.addLayout(axis_x, 1)
 h_layout.addLayout(axis_y, 1)
 h_layout.addLayout(axis_z, 1)
 
+# Ajout du layout horizontal dans le layout vertical 
 layout.addLayout(h_layout)
 
 
+
 # Creation du bouton com
-valOM1 = QComboBox()
-valOM1.addItems(OM1_list)
-valOM1.setCurrentText("Sélectionnez le port")
-valOM1.setStyleSheet("background-color: ChartThemeDark; color: white font-size: 18px;")
-#valOM1.setFixedHeight(40)
-layout.addWidget(valOM1)
+OM1 = QComboBox(window)
+valOM1 = OM1.currentText()
+OM1.addItem("Selectionnez le port")
+ports = serial.tools.list_ports.comports()
+OM1_list = []
+for p,d,h in sorted(ports):
+    OM1_list.append(p)
+OM1.setStyleSheet("background-color: ChartThemeDark; color: white font-size: 18px;")
+layout.addWidget(OM1)
+
 
 
 # Creation du bouton commencer
@@ -126,9 +139,37 @@ button_stop.setStyleSheet("background-color: green; color: white;")
 button_stop.setPalette(palette)
 # layout.addWidget(button_stop) # je lai pas ajouter au layer encore mais il est la 
 
+# Creation du bouton effacer 
+clear_button = QPushButton("Effacer", window)
+clear_button.setStyleSheet("background-color: ChartThemeDark; color: white;")
+clear_button.setPalette(palette)
+layout.addWidget(clear_button)
+
+# Creation de la zone de texte pour la sauvegarde
+save_text = QLineEdit(window)
+save_text.setPlaceholderText("Nom de fichier...")
+save_text.setStyleSheet("background-color: ChartThemeDark; color: white;")
+
+# Creation du bouton sauvegarder
+save_button = QPushButton("Enregistrer", window)
+save_button.setStyleSheet("background-color: green; color: white;")
+save_button.setPalette(palette)
+
+
+
+# Ajout du bouton et de la zone de texte
+save_layout = QHBoxLayout()
+save_layout.addWidget(save_text)
+save_layout.addWidget(save_button)
+layout.addLayout(save_layout)
+
+
+
 # legende du graph
 legend = chart.legend()
 legend.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+
 
 # Axe X
 axis_x = QValueAxis()
@@ -150,9 +191,47 @@ axis_y.setGridLineVisible(True)
 axis_y.setGridLineColor(Qt.GlobalColor.gray)
 chart.addAxis(axis_y, Qt.AlignmentFlag.AlignLeft)
 
+
+
+# Ajoute les series 1, 2 et 3 
 chart.addSeries(series1)
 chart.addSeries(series2)
 chart.addSeries(series3)
+
+
+
+# Création du timer
+timer = QTimer()
+timer.setInterval(1000)  # Timer qui s'exécute toutes les secondes
+timer_label = QLabel("00:00:00")
+timer_label.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+# Mise à jour du temps écoulé
+elapsed_time = QTime(0, 0, 0)
+def update_timer():
+    global elapsed_time
+    elapsed_time = elapsed_time.addSecs(1)
+    timer_label.setText(elapsed_time.toString("hh:mm:ss"))
+
+# Connexion du timer à la fonction d'actualisation
+timer.timeout.connect(update_timer)
+
+# Ajout du timer à l'interface utilisateur
+timer_layout = QHBoxLayout()
+timer_layout.addStretch(1)
+timer_layout.addWidget(timer_label)
+layout.addLayout(timer_layout)
+
+
+# Fonction qui réinitialise le timer
+def reset_timer():
+    timer.stop()
+    timer_label.setText("00:00:00")
+    global elapsed_time
+    elapsed_time = QTime(0, 0, 0) # Mettre à jour la variable elapsed_time en la remettant à 0
+
+
+
 
 # Fonction qui affiche les points sur le graphique
 def show_curve():
@@ -182,33 +261,6 @@ def show_curve():
 
 
 
-# Creation du bouton effacer 
-clear_button = QPushButton("Effacer", window)
-clear_button.setStyleSheet("background-color: ChartThemeDark; color: white;")
-clear_button.setPalette(palette)
-layout.addWidget(clear_button)
-
-# Creation de la zone de texte pour la sauvegarde
-save_text = QLineEdit(window)
-save_text.setPlaceholderText("Nom de fichier...")
-save_text.setStyleSheet("background-color: ChartThemeDark; color: white;")
-
-# Creation du bouton sauvegarder
-save_button = QPushButton("Enregistrer", window)
-save_button.setStyleSheet("background-color: green; color: white;")
-save_button.setPalette(palette)
-
-
-
-# Ajout du bouton et de la zone de texte
-save_layout = QHBoxLayout()
-save_layout.addWidget(save_text)
-save_layout.addWidget(save_button)
-layout.addLayout(save_layout)
-
-
-
-
 # Fonction qui enregistre l'image avec le nom entré
 def save_image():
     # nom du dossier
@@ -222,37 +274,6 @@ def save_image():
     chartview.grab().save((chemin_vers_dossier), b"JPG")
 
 
-
-# Création du timer
-timer = QTimer()
-timer.setInterval(1000)  # Timer qui s'exécute toutes les secondes
-
-timer_label = QLabel("00:00:00")
-timer_label.setAlignment(Qt.AlignmentFlag.AlignRight)
-
-# Mise à jour du temps écoulé
-elapsed_time = QTime(0, 0, 0)
-def update_timer():
-    global elapsed_time
-    elapsed_time = elapsed_time.addSecs(1)
-    timer_label.setText(elapsed_time.toString("hh:mm:ss"))
-
-# Connexion du timer à la fonction d'actualisation
-timer.timeout.connect(update_timer)
-
-# Ajout du timer à l'interface utilisateur
-timer_layout = QHBoxLayout()
-timer_layout.addStretch(1)
-timer_layout.addWidget(timer_label)
-layout.addLayout(timer_layout)
-
-
-# Fonction qui réinitialise le timer
-def reset_timer():
-    timer.stop()
-    timer_label.setText("00:00:00")
-    global elapsed_time
-    elapsed_time = QTime(0, 0, 0) # Mettre à jour la variable elapsed_time en la remettant à 0
 
 def clear_clicked():
     # Afficher la fenêtre de confirmation
@@ -273,7 +294,7 @@ def clear_clicked():
         update_info_z()
 
 
-# Fonction qui permet d'afficher les informations lier au graphique
+# Fonction qui permet d'afficher les informations lier au graphique dans les boxs x,y et z 
 def update_info_x():
     axis_x_box.clear()
     axis_x_box.insertPlainText("")
@@ -290,18 +311,7 @@ def update_info_z():
     for i in range(series3.count()):
         axis_z_box.insertPlainText("Point {}: ({}, {})\n".format(i, series3.at(i).x(), series3.at(i).y()))
 
-# Lien entre le bouton "Commencer" et la fonction d'affichage des points et de l'updade des info du graphique 
-button.clicked.connect(show_curve)        
-button.clicked.connect(update_info_x)
-button.clicked.connect(update_info_y)
-button.clicked.connect(update_info_z)
 
-# Lien entre le bouton "Effacer" et la fonction pour nettoyer le graphique et de l'updade des info du graphique 
-clear_button.clicked.connect(clear_clicked)
-clear_button.clicked.connect(update_info_x)
-clear_button.clicked.connect(update_info_y)
-clear_button.clicked.connect(update_info_z)
-clear_button.clicked.connect(reset_timer)
 
 def save_excel():
     # Disable the save button and change its color to gray
@@ -375,12 +385,33 @@ def save_excel():
     workbook.close()
     
    
+# Lien entre le bouton "Commencer" et la fonction d'affichage des points et de l'updade des info du graphique 
+button.clicked.connect(show_curve)        
+button.clicked.connect(update_info_x)
+button.clicked.connect(update_info_y)
+button.clicked.connect(update_info_z)
 
+# Lien entre le bouton "Effacer" et la fonction pour nettoyer le graphique et de l'updade des info du graphique 
+clear_button.clicked.connect(clear_clicked)
+clear_button.clicked.connect(update_info_x)
+clear_button.clicked.connect(update_info_y)
+clear_button.clicked.connect(update_info_z)
+clear_button.clicked.connect(reset_timer)
+
+# Lien entre le bouton "sauvegarder" et les fonctions pour sauvegarder en fichier exel et image
 save_button.clicked.connect(save_excel)
 save_button.clicked.connect(save_image)
+
+
+
+# Met a jour le graphique 
 chart.scaleChanged.connect(update_info_x)
 chart.scaleChanged.connect(update_info_y) 
 chart.scaleChanged.connect(update_info_z) 
+
+
+
+# Ajoute le layout a la fenetre et affiche la fenetre
 window.setLayout(layout)
 window.show()
 sys.exit(app.exec())
